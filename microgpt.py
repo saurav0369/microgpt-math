30967e86aa8c791b8c5e905fed1dbb2854d3a765
sauravsharma <sauravsharma0369@gmail.com>
2026-03-22 16:09:51 +0530

    infrence successful

--- microgpt.py
+++ microgpt.py
@@ -24,45 +24,42 @@ print (f"vocab_size {vocab_size}")
 
 #value class 
 
-class Value :
-    Value = 6
+class Value:
 
-    __slot__ =('data', 'grad', '_children', '_local_grads')
+    __slots__ = ('data', 'grad', '_children', '_local_grads')
 
-    def __init__(self, data, children =(),_local_grads=()):
-        self.data = data 
-        self.grad = 0 
-        self._children = children 
-        self._local_grad = _local_grads
+    def __init__(self, data, children=(), _local_grads=()):
+        self.data = data
+        self.grad = 0
+        self._children = children
+        self._local_grads = _local_grads
 
     def __add__(self, other):
-        other = other if isinstance (other, value) else Value(other)
-        return Value (self.data + other.data (self, other), (1,1))
+        other = other if isinstance(other, Value) else Value(other)
+        return Value(self.data + other.data, (self, other), (1, 1))
 
     def __mul__(self, other):
-        other = other if isinstance (other, value) else value(other)
-        return Value (self.data * other.data (self, other) (other.data , self.data))
+        other = other if isinstance(other, Value) else Value(other)
+        return Value(self.data * other.data, (self, other), (other.data, self.data))
 
-    def relu(self): return Value (max(0,self.data), (self,), (float(self.data > 0)))
+    def relu(self): return Value(max(0, self.data), (self,), (float(self.data > 0),))
 
-    def exp(self): return Value (math.exp(self.data), (self,), (math.exp(self.data),))
+    def exp(self): return Value(math.exp(self.data), (self,), (math.exp(self.data),))
+
+    def log(self): return Value(math.log(self.data), (self,), (1.0 / self.data,))
 
     def __pow__(self, other):
-        return Value (self.data ** other, (self,), (other * self.data**(other-1)))
+        return Value(self.data ** other, (self,), (other * self.data ** (other - 1),))
 
-    def __neg__ (self): return self * -1
+    def __neg__(self): return self * -1
     def __sub__(self, other): return self + (-other)
-    def __truediv (self, other): return self * other ** -1 
+    def __truediv__(self, other): return self * other ** -1
 
-    def __radd__(self, other): self + other 
-    def __rsub__(self, other): other + (-self)
+    def __radd__(self, other): return self + other
+    def __rsub__(self, other): return other + (-self)
 
-    def __rmul__ (self, other): return self * other 
-    def __rtruediv(self, other): return other * self ** -1 
-
-    print(__truediv)
-    print (__pow__)
-    print (__add__)
+    def __rmul__(self, other): return self * other
+    def __rtruediv__(self, other): return other * self ** -1
 
 
     def backward(self):
@@ -101,10 +98,10 @@ state_dict ={ 'wte': matrix(vocab_size, n_embd), 'wpe': matrix(block_size, n_emb
 'lm_head': matrix(vocab_size, n_embd)}
 
 for i in range (n_layer):
-    state_dict[f'layer{i}.attn_wq'] = matrix (n_embd, n_embd)
-    state_dict[f'layer{i}.attn.wk'] = matrix (n_embd, n_embd)
-    state_dict[f'layer{i}.attn.wv'] = matrix (n_embd, n_head)
-    state_dict[f'layer{i}.attn.wo'] = matrix (n_embd, n_embd)
+    state_dict[f'layer{i}.attn_wq'] = matrix(n_embd, n_embd)
+    state_dict[f'layer{i}.attn_wk'] = matrix(n_embd, n_embd)
+    state_dict[f'layer{i}.attn_wv'] = matrix(n_embd, n_head)
+    state_dict[f'layer{i}.attn_wo'] = matrix(n_embd, n_embd)
 
     state_dict[f'layer{i}.mlp_fc1'] = matrix(4 * n_embd, n_embd)
     state_dict[f'layer{i}.mlp_fc2'] = matrix (n_embd, 4 * n_embd)
@@ -119,11 +116,16 @@ for mat in state_dict.values():
 
 print (f'params numbers{len(params)}')            
 
-#linear 
-def linear (x,w):
-    return [sum(wi*xi for wi,xi in zip(wo,x)) for wo in x ]
+#linear
+def linear(x, w):
+    return [sum(wi * xi for wi, xi in zip(wo, x)) for wo in w]
 
-#softmax 
+#rmsnorm
+def rmsnorm(x):
+    rms = (sum(xi * xi for xi in x) * (1.0 / len(x))) ** 0.5
+    return [xi / rms for xi in x]
+
+#softmax
 
 def softmax(logits):
     max_value = max(val.data for val in logits)
@@ -145,9 +147,9 @@ def gpt(token_id, pos_id, key, value):
         
     
 
-        q = linear (x, state_dict [f'layer{li}.attn_wq'])
-        k = linear (x, state_dict [f'layer{li}.attn_wk'])
-        v = linear (x, state_dict [f'layer{li}.attn_wv'])
+        q = linear(x, state_dict[f'layer{li}.attn_wq'])
+        k = linear(x, state_dict[f'layer{li}.attn_wk'])
+        v = linear(x, state_dict[f'layer{li}.attn_wv'])
 
         keys[li].append(k)
         values[li].append(v)
@@ -160,7 +162,7 @@ def gpt(token_id, pos_id, key, value):
             hs = h * head_dim
             q_h = q [hs: hs + head_dim] 
             k_h = [ki [ hs:hs + head_dim] for ki in keys[li]]
-            v_h = [vi [ hs:hs + head_dim] for vi in value [li]]
+            v_h = [vi[hs:hs + head_dim] for vi in values[li]]
             attn_logits = [sum(q_h[j] * k_h[t][j] for j in range(head_dim)) / head_dim**0.5 for t in range(len(k_h))]
             attn_weights = softmax(attn_logits)
             head_out = [sum(attn_weights[t] * v_h[t][j] for t in range(len(v_h))) for j in range(head_dim)]
@@ -176,49 +178,46 @@ def gpt(token_id, pos_id, key, value):
         x = linear(x, state_dict[f'layer{li}.mlp_fc2'])
         x = [a + b for a, b in zip(x, x_residual)]
 
-        logits = linear(x, state_dict['lm_head'])
-        return logits
+    logits = linear(x, state_dict['lm_head'])
+    return logits
 
-learning_rate, beta1, beta2, eps_adam = 0.01, 0.85, 0.99, 1e-8
-m = [0.0] * len(params) # first moment buffer
-v = [0.0] * len(params) # second moment buffer
+learning_rate , beta1, beta2 , eps_adam = 0.01, 0.85, 0.99, 1e-8
+m = [0.0] * len(params)
+v_adam = [0.0] * len(params)
 
-num_steps = 1000 # number of training steps
+
+num_steps = 1000
 for step in range(num_steps):
-
-    # Take single document, tokenize it, surround it with BOS special token on both sides
-    doc = docs[step % len(docs)]
+    doc = docs[step% len(docs)]
     tokens = [BOS] + [uchars.index(ch) for ch in doc] + [BOS]
     n = min(block_size, len(tokens) - 1)
 
-    # Forward the token sequence through the model, building up the computation graph all the way to the loss
     keys, values = [[] for _ in range(n_layer)], [[] for _ in range(n_layer)]
     losses = []
+
     for pos_id in range(n):
         token_id, target_id = tokens[pos_id], tokens[pos_id + 1]
         logits = gpt(token_id, pos_id, keys, values)
         probs = softmax(logits)
         loss_t = -probs[target_id].log()
         losses.append(loss_t)
-    loss = (1 / n) * sum(losses) # final average loss over the document sequence. May yours be low.
+    loss = (1 / n) * sum(losses)
 
-    # Backward the loss, calculating the gradients with respect to all model parameters
     loss.backward()
 
-    # Adam optimizer update: update the model parameters based on the corresponding gradients
-    lr_t = learning_rate * (1 - step / num_steps) # linear learning rate decay
+    lr_t = learning_rate * (1 - step / num_steps) 
     for i, p in enumerate(params):
         m[i] = beta1 * m[i] + (1 - beta1) * p.grad
-        v[i] = beta2 * v[i] + (1 - beta2) * p.grad ** 2
+        v_adam[i] = beta2 * v_adam[i] + (1 - beta2) * p.grad ** 2
         m_hat = m[i] / (1 - beta1 ** (step + 1))
-        v_hat = v[i] / (1 - beta2 ** (step + 1))
+        v_hat = v_adam[i] / (1 - beta2 ** (step + 1))
         p.data -= lr_t * m_hat / (v_hat ** 0.5 + eps_adam)
         p.grad = 0
 
     print(f"step {step+1:4d} / {num_steps:4d} | loss {loss.data:.4f}", end='\r')
 
-# Inference: may the model babble back to us
-temperature = 0.5 # in (0, 1], control the "creativity" of generated text, low to high
+
+temperature = 0.5 
 print("\n--- inference (new, hallucinated names) ---")
 for sample_idx in range(20):
     keys, values = [[] for _ in range(n_layer)], [[] for _ in range(n_layer)]
@@ -232,4 +231,3 @@ for sample_idx in range(20):
             break
         sample.append(uchars[token_id])
     print(f"sample {sample_idx+1:2d}: {''.join(sample)}")
-
